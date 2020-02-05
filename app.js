56e45dc9573c4568cf5e108e17cb5fd4741341e2
Jordan Nelson <jordannelson@protonmail.com>
2020-02-05 11:42:05 -0600

    fix directory refs

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-let fs = require("fs");
-const mkdirp = require("mkdirp");
+let fs = require('fs');
+let mkdirp = require('mkdirp');
+let path = require('path');
 
 let args = process.argv.slice(2);
+let packageName = "";
 
-const packageName = "";
+const dirJSON = path.join(__dirname, '..', '..', 'package.json');
 
 if (args[0]) {
     packageName = args[0];    
 }
 else {
-    new Error("Must provide a package name as an argument.\n" +
+    throw new Error("Must provide a package name as an argument.\n" +
     "\tExample: node unitynpminit COMPANY_NAME.PACKAGENAME");
-    process.exit(1);
 }
 
-// verify project directory structure
-var src = path.join(__dirname, '..', 'Assets', package);
+const dirSrc = path.join(__dirname, '..', '..', 'Assets', packageName);
 
-if (!src) {
-    mkdirp(src, function(err) {
+// verify project directory structure
+
+console.log(dirSrc);
+if (!fs.existsSync(dirSrc)) {
+    console.log("dir not found");
+	mkdirp(dirSrc, function(err) {
         if (err) {
             console.error(err);
             process.exit(1);
@@ -26,10 +30,12 @@ if (!src) {
     });
 }
 
+console.log("Aw shit.");
+
 // edit package.json
 packageJSON = JSON.parse(
     fs.readFileSync(
-        path.join(__dirname, '..', 'package.json')
+        dirJSON
     )
 );
 
@@ -43,11 +49,19 @@ packageJSON.scripts["postinstall"] =
     "node scripts/postinstall.js " + packageName.toString();
 
 // files
-packageJSON.files.push("Assets/" + package);
+if (packageJSON.files) {
+    packageJSON.files.push("Assets/" + packageName);
+}
+else {
+    packageJSON['files'] = [];
+    packageJSON['files'].push("Assets/" + packageName);
+}
 
 // dependencies
 packageJSON.dependencies["ncp"] = "^2.0.0";
 packageJSON.dependencies["mkdirp"] = "^0.5.1";
+
+fs.writeFileSync(dirJSON, JSON.stringify(packageJSON));
 
 
 
