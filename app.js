150ee57aa628c13e7d3a994b261f083d3016e424
Jordan Nelson <jordannelson@protonmail.com>
2020-02-07 14:04:43 -0600

    use process execution dir as root

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 
-console.log("Initializing Unity project as npm package.");
-console.log(process.cwd());
-
 let fs = require('fs');
 let mkdirp = require('mkdirp');
 let path = require('path');
 
+console.log("Initializing Unity project as npm package.");
+
+// Get package root directory and file name
+let rootDir = process.cwd();
+let rootFile = path.basename(rootDir);
+
+// Get package name
 let args = process.argv.slice(2);
 let packageName = "";
-
-const dirJSON = path.join(__dirname, '..', '..', 'package.json');
 
 if (args[0]) {
     packageName = args[0];    
 }
 else {
-    throw new Error("Must provide a package name as an argument.\n" +
-    "\tExample: node unitynpminit COMPANY_NAME.PACKAGENAME");
+    console.log("Package name not provided. Setting package name to "
+        + " project root file name.");
+    packageName = rootFile;
 }
 
-const dirSrc = path.join(__dirname, '..', '..', 'Assets', packageName);
-
 // verify project directory structure
+const dirSrc = path.join(rootDir, 'Assets', packageName);
+
 if (!fs.existsSync(dirSrc)) {
     console.log("Source directory, " + dirSrc + " not found. Creating"
         + " source directory.");
@@ -34,28 +37,35 @@ if (!fs.existsSync(dirSrc)) {
     });
 }
 
-// get package.json as object if it exists
-if (fs.existsSync(dirJSON)) {
+// Get or create package.json
+const dirJSON = "";
+
+if (fs.existsSync(path.join(rootDir, 'package.json'))) {
     packageJSON = JSON.parse(
         fs.readFileSync(
-            dirJSON
+            rootDir, "package.json"
         )
     );
 }
-// else create new object for package.json
 else {
     packageJSON = {
         name: packageName.toString()
     };
 }
 
+// create scripts folder
+
+// copy postinstall.js to scripts folder
+
 // set postinstall script for unity package
-packageJSON.scripts["postinstall"] =
+
+/*packageJSON.scripts["postinstall"] =
     "node scripts/postinstall.js " + packageName.toString();
+*/
 
 // add file list to package.json
 if (packageJSON.files) {
-    packageJSON.files.push("Assets/" + packageName);
+    packageJSON.files.push('Assets/' + packageName);
 }
 else {
     packageJSON['files'] = [];
@@ -63,8 +73,8 @@ else {
 }
 
 // dependencies
-packageJSON.dependencies["ncp"] = "^2.0.0";
-packageJSON.dependencies["mkdirp"] = "^0.5.1";
+packageJSON.dependencies['ncp'] = '^2.0.0';
+packageJSON.dependencies['mkdirp'] = '^0.5.1';
 
 // write edits to package.json
 fs.writeFileSync(dirJSON, JSON.stringify(packageJSON));
